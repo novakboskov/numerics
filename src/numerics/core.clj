f6040ca739f6a38cfa915bf938ab30b5ecf27e53
Novak Boskov <gnovak.boskov@gmail.com>
2015-10-16 15:42:24 +0200

    overd-pseudo-inverse and its testing

--- src/numerics/core.clj
+++ src/numerics/core.clj
@@ -25,7 +25,7 @@
   "Suppose y = M*x and assume n<m.
   In this case there are fewer constraints than unknowns, and the system is underdetermined, with an infinite number of solutions.
   We can pick one of these solutions by finding the smallest one. That is, we will minimize x subject to the constraint y = M*x. The method of Lagrange multipliers has us add a term to the quantity to be minimized:
-  |x|^2 + lambda^T * (y − M*x)
+  ||x||^2 + lambda^T * (y − M*x)
   Differentiting w.r.t x and setting the result equal to zero yields
   2*x − M^T * lambda = 0
   We can’t just solve for lambda since M is not a square matrix, but we can premultiply by M to obtain:
@@ -44,3 +44,22 @@
                     (cm/mmul coeficients)
                     cm/inverse)
                constraints)))
+
+(defn overd-pseudo-inverse
+  "Suppose y = M*x and assume n>m.
+  In this case there are more constraints than unknowns, and the system is overdetermined, with no solutions (except for degenerate cases).
+  We can find a least-squares solution that minimizes the error (y − M*x). We want to find x that minimizes:
+  ||y − M*x||^2 , or:
+  (y − M*x)^T * (y − M*x), or:
+  y^T * y − y^T * M*x − x^T * M^T * y + x^T * M^T * M*x
+  Differentiating w.r.t. x and setting the result equal to zero yields:
+  −(y^T * M )^T − (M^T * y) + 2*M^T * M*x = 0, so:
+  x = (M^T * M)^−1 * M^T * y
+  where (M^T * M)^−1 * M^T (a m by n matrix) is called a pseudo-inverse."
+  [coeficients constants]
+  (cm/mmul (-> coeficients
+               cm/transpose
+               (cm/mmul coeficients)
+               cm/inverse)
+           (cm/transpose coeficients)
+           constants))
--- test/numerics/core_test.clj
+++ test/numerics/core_test.clj
@@ -21,4 +21,16 @@
     ;; 100g eggs with peas included
     (is (=
          (underd-pseudo-inverse [[0.499 0.0006 0.1445 -0.3] [0.039 0.8111 0.004 -0.3] [0.085 0.0085 0.0542 -0.4]] [-0.0077 -0.0994 -0.1258])
-         [0.19532029406891444 4.5078579368261484E-4 0.015786666292379 0.3581542349703774]))))
+         [0.19532029406891444 4.5078579368261484E-4 0.015786666292379 0.3581542349703774]))
+    ;; 100g bread with n by n problem, underd-pseudo-inverse-test can solve n by n problems
+    ;; the results are slightly differ
+    (is (=
+         (underd-pseudo-inverse [[0.0077 0.0006 -0.3] [0.1258 0.0085 -0.4] [0.0994 0.8111 -0.3]] [-0.499 -0.08 -0.03])
+         [5.065993120613627 0.0054885019614173 1.7933714670996652]))))
+
+(deftest overd-pseudo-inverse-test
+  (testing "overd-pseudo-inverse testing"
+    ;; 100g bread, eggs only
+    (is (=
+         (overd-pseudo-inverse [[0.0077 -0.3] [0.1258 -0.4] [0.0994 -0.3]] [-0.499 -0.085 -0.039])
+         [5.0202342161418 1.7921126797673275]))))
